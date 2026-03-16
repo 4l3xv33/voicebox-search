ec6d73958992222e7272d3783e4a558ea815ae2b
4l3xv33 <207385389+4l3xv33@users.noreply.github.com>
2026-03-16 06:03:45 -0400

    first commit

---
+++ documents.js
@@ -0,0 +1,27 @@
+const DOCUMENTS = [
+  {
+    filename: "star_spangled_banner.pdf",
+    filelink: "https://www.loc.gov/item/ihas.100010361",
+    filecontent: "O say can you see, by the dawn's early light, What so proudly we hailed at the twilight's last gleaming, Whose broad stripes and bright stars through the perilous fight, O'er the ramparts we watched, were so gallantly streaming? And the rocket's red glare, the bombs bursting in air, Gave proof through the night that our flag was still there; O say does that star-spangled banner yet wave O'er the land of the free and the home of the brave?"
+  },
+  {
+    filename: "america_the_beautiful.pdf",
+    filelink: "https://www.loc.gov/item/ihas.100010365",
+    filecontent: "O beautiful for spacious skies, For amber waves of grain, For purple mountain majesties Above the fruited plain! America! America! God shed His grace on thee, And crown thy good with brotherhood From sea to shining sea! O beautiful for pilgrim feet Whose stern impassioned stress, A thoroughfare for freedom beat Across the wilderness! America! America! God mend thine every flaw, Confirm thy soul in self-control, Thy liberty in law! O beautiful for heroes proved In liberating strife, Who more than self their country love And mercy more than life! America! America! May God thy gold refine Till all success be nobleness, And every gain divine! O beautiful for patriot dream That sees beyond the years, Thine alabaster cities gleam Undimmed by human tears! America! America! God shed His grace on thee, And crown thy good with brotherhood From sea to shining sea! Oh beautiful for halcyon skies For amber waves of grain For purple mountain majesties Above the enameled plain! America! America! God shed His grace on thee, Till souls wax fair as earth and air And music-hearted sea! O beautiful for pilgrim feet Whose stern impassioned stress, A thoroughfare for freedom beat Across the wilderness! America! America! God shed His grace on thee, Till paths be wrought through wilds of thought By pilgrims foot and knee! Oh beautiful for glory-tale Of liberating strife, When once and twice for man’s avail Men lavished precious life! America! America! God shed His grace on thee, Till selfish gain no longer strain The banner of the free! O beautiful for patriot dream That sees beyond the years, Thine alabaster cities gleam Undimmed by human tears! America! America! God shed His grace on thee, Till nobler men keep once again Thy whiter jubilee!"
+  },
+  {
+    filename: "god_bless_america.pdf",
+    filelink: "https://www.loc.gov/item/ihas.100010433",
+    filecontent: "While the storm clouds gather far across the sea, Let us swear allegiance to a land that's free, Let us all be grateful for a land so fair, As we raise our voices in a solemn prayer. God Bless America, Land that I love. Stand beside her, and guide her Thru the night with a light from above. From the mountains, to the prairies, To the oceans, white with foam God bless America, My home sweet home."
+  },
+  {
+    filename: "my_country_tis_of_thee.pdf",
+    filelink: "https://www.loc.gov/item/ihas.100010436",
+    filecontent: "My Country Tis of Thee Sweet land of liberty of thee I sing Land where my father’s died Land of the pilgrim's pride From every mountain side Let freedom ring My native country, thee, Land of the noble free, Thy name I love; I love thy rocks and rills, Thy woods and templed hills; My heart with rapture thrills, Like that above. Our fathers' God to thee, Author of liberty, To thee we sing. Long may our land be bright, With freedom's holy light, Protect us by thy might, Great God our King!"
+  },
+  {
+    filename: "this_land_is_your_land.pdf",
+    filelink: "https://www.loc.gov/item/ihas.100010446",
+    filecontent: "This land is your land, and this land is my land From California to the New York Island From the Redwood Forest to the Gulf Stream Waters This land was made for you and me As I went walking that ribbon of highway And I saw above me that endless skyway I saw below me that golden valley This land was made for you and me I roamed and rambled and I followed my footsteps To the sparkling sands of her diamond deserts All around me a voice was a-sounding This land was made for you and me There was a big high wall there that tried to stop me Sign was painted, said, Private Property But on the back side, it didn't say nothing This land was made for you and me When the sun comes shining, then I was strolling And the wheat fields waving and the dust clouds rolling A voice come chanting as the fog was lifting This land was made for you and me This land is your land, and this land is my land From California to the New York Island From Redwood Forest to the Gulf Stream Waters This land was made for you and me"
+  }
+];
